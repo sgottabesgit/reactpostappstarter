1d906fdb2e3e6cae6cab48887f379a68b755d2da
Sgottabe <sgottabe@gmail.com>
2024-02-25 21:57:24 -0700

    edited landing page

--- frontend/src/pages/Landing/Landing.page.jsx
+++ frontend/src/pages/Landing/Landing.page.jsx
@@ -3,7 +3,9 @@ import { Container } from "@mantine/core";
 const Landing = () => {
   return (
     <Container>
-      <h1>Welcome to the homepage. Anyone can see this page</h1>
+      <h1>Welcome to our photo-sharing app! Share your beautiful moments with the world.</h1>
+      {/* Add images or any other visual elements */}
+      <img src="your-image-url.jpg" alt="App Showcase" />
     </Container>
   );
 };
