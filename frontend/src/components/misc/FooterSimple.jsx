265109072550f0b5e6d00a9b18457199b27f74a6
Sgottabe <sgottabe@gmail.com>
2024-02-27 02:28:58 -0700

    added carera svg to footer

--- frontend/src/components/misc/FooterSimple.jsx
+++ frontend/src/components/misc/FooterSimple.jsx
@@ -3,6 +3,8 @@ import React from 'react';
 import { Container, Group, Anchor } from '@mantine/core';
 import { MantineLogo } from '@mantinex/mantine-logo'; // Assuming this is the correct import path for MantineLogo
 import classes from './FooterSimple.module.css';
+import SVGComponent from "./svgviewer-react-output";
+
 
 const links = [
     { link: '#', label: 'Contact' },
@@ -27,7 +29,7 @@ const FooterSimple = () => {
     return (
         <div className={classes.footer}>
             <Container className={classes.inner}>
-                <MantineLogo size={28} />
+                <SVGComponent />
                 <Group className={classes.links}>{items}</Group>
             </Container>
         </div>
