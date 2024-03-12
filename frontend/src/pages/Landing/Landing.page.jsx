d89f978df40f3beadcc88d13dd403db88106b171
Sgottabe <sgottabe@gmail.com>
2024-03-12 01:03:50 -0600

    added images to landing page

--- frontend/src/pages/Landing/Landing.page.jsx
+++ frontend/src/pages/Landing/Landing.page.jsx
@@ -4,10 +4,9 @@ import { ArticleCardImage } from "../../components/misc/ArticleCardImage";
 import '@mantine/carousel/styles.css';
 
 const articles = [
-  { id: 1, title: 'Article 1', category: 'Technology', image: 'image-url-1.jpg' },
-  { id: 2, title: 'Article 2', category: 'Science', image: 'image-url-2.jpg' },
-  { id: 3, title: 'Article 3', category: 'Travel', image: 'image-url-3.jpg' },
-  // Add more articles as needed
+  { id: 1, title: 'Prosthetic arm', category: 'Technology', image: 'https://images.unsplash.com/photo-1581090464777-f3220bbe1b8b?q=80&w=2432&auto=format&fit=crop&ixlib=rb-4.0.3&ixid=M3wxMjA3fDB8MHxwaG90by1wYWdlfHx8fGVufDB8fHx8fA%3D%3D' },
+  { id: 2, title: 'DNA', category: 'Science', image: 'https://images.unsplash.com/photo-1628595351029-c2bf17511435?q=80&w=2832&auto=format&fit=crop&ixlib=rb-4.0.3&ixid=M3wxMjA3fDB8MHxwaG90by1wYWdlfHx8fGVufDB8fHx8fA%3D%3D' },
+  { id: 3, title: 'Banff', category: 'Nature', image: 'https://images.unsplash.com/photo-1709568039915-14baf8fbba66?q=80&w=2940&auto=format&fit=crop&ixlib=rb-4.0.3&ixid=M3wxMjA3fDB8MHxwaG90by1wYWdlfHx8fGVufDB8fHx8fA%3D%3D' },
 ];
 
 const Landing = () => {
