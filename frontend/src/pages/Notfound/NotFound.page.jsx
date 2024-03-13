e13735583cb6f11f9b5ee86255433e6aa1968f72
Sgottabe <sgottabe@gmail.com>
2024-03-13 02:43:28 -0600

    imported link component

--- frontend/src/pages/Notfound/NotFound.page.jsx
+++ frontend/src/pages/Notfound/NotFound.page.jsx
@@ -1,7 +1,15 @@
+import { Link } from 'react-router-dom';
+
+
 const NotFound = () => {
   return (
     <div>
-      <h1>Page not found</h1>
+      <h1>OOPS! Page not found</h1>
+      <p>Looks like you've wandered off the beaten path.</p>
+      <Link to="/" className="not-found-link">
+        Go back to Home
+      </Link>
+
     </div>
   );
 };
