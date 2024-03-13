0a5f891e14514afebdcc77e4e7be3a577ac918b9
Sgottabe <sgottabe@gmail.com>
2024-03-13 03:10:25 -0600

    custom 404 page

--- frontend/src/pages/Notfound/NotFound.page.jsx
+++ frontend/src/pages/Notfound/NotFound.page.jsx
@@ -1,15 +1,16 @@
 import { Link } from 'react-router-dom';
-
+import styles from './NotFound.page.module.css'; // Import CSS module for styling
 
 const NotFound = () => {
   return (
-    <div>
-      <h1>OOPS! Page not found</h1>
-      <p>Looks like you've wandered off the beaten path.</p>
-      <Link to="/" className="not-found-link">
-        Go back to Home
-      </Link>
-
+    <div className={styles.notFoundContainer}>
+      <div className={styles.notFoundText}>
+        <h1 className={styles.notFoundHeading}>Oops! Page not found</h1>
+        <p className={styles.notFoundDescription}>
+          Looks like you've wandered off the beaten path!
+        </p>
+        <Link to="/" className={styles.notFoundLink}>Go back to Home</Link>
+      </div>
     </div>
   );
 };
