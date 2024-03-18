2f9d7b072adeacc07c4f22034c1a1320154aebdb
Sgottabe <sgottabe@gmail.com>
2024-03-18 00:08:56 -0600

    posts show in carsouel

--- frontend/src/pages/Landing/Landing.page.jsx
+++ frontend/src/pages/Landing/Landing.page.jsx
@@ -1,25 +1,36 @@
+import { useEffect, useState } from "react";
 import { Container } from "@mantine/core";
-import { Carousel } from '@mantine/carousel';
+import { Carousel } from "@mantine/carousel";
 import { ArticleCardImage } from "../../components/misc/ArticleCardImage";
+import axios from "axios";
 import '@mantine/carousel/styles.css';
 
-const articles = [
-  { id: 1, title: 'Prosthetic arm', category: 'Technology', image: 'https://images.unsplash.com/photo-1581090464777-f3220bbe1b8b?q=80&w=2432&auto=format&fit=crop&ixlib=rb-4.0.3&ixid=M3wxMjA3fDB8MHxwaG90by1wYWdlfHx8fGVufDB8fHx8fA%3D%3D' },
-  { id: 2, title: 'DNA', category: 'Science', image: 'https://images.unsplash.com/photo-1628595351029-c2bf17511435?q=80&w=2832&auto=format&fit=crop&ixlib=rb-4.0.3&ixid=M3wxMjA3fDB8MHxwaG90by1wYWdlfHx8fGVufDB8fHx8fA%3D%3D' },
-  { id: 3, title: 'Banff', category: 'Nature', image: 'https://images.unsplash.com/photo-1709568039915-14baf8fbba66?q=80&w=2940&auto=format&fit=crop&ixlib=rb-4.0.3&ixid=M3wxMjA3fDB8MHxwaG90by1wYWdlfHx8fGVufDB8fHx8fA%3D%3D' },
-];
-
 const Landing = () => {
+  const [posts, setPosts] = useState([]);
+
+  useEffect(() => {
+    const fetchPosts = async () => {
+      try {
+        const response = await axios.get("http://localhost:8085/api/posts");
+        setPosts(response.data);
+      } catch (error) {
+        console.error("Error fetching posts:", error);
+      }
+    };
+
+    fetchPosts();
+  }, []);
+
   return (
     <Container>
       <Carousel withIndicators height={200}>
-        {articles.map((article) => (
-          <Carousel.Slide key={article.id}>
+        {posts.map((post) => (
+          <Carousel.Slide key={post.id}>
             <ArticleCardImage
-              title={article.title}
-              category={article.category}
-              image={article.image}
-              id={article.id}
+              title={post.title}
+              category={post.category}
+              image={post.image}
+              id={post.id}
             />
           </Carousel.Slide>
         ))}
