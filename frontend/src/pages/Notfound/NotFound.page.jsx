9c9aae7b10017ef6296f534b8a08e9533f05cf46
Sgottabe <sgottabe@gmail.com>
2024-03-18 01:35:02 -0600

    added 404 image

--- frontend/src/pages/Notfound/NotFound.page.jsx
+++ frontend/src/pages/Notfound/NotFound.page.jsx
@@ -1,9 +1,13 @@
 import { Link } from 'react-router-dom';
 import styles from './NotFound.page.module.css'; // Import CSS module for styling
+import errorImage from '../../assets/error.jpg'; // Import your error image
 
 const NotFound = () => {
   return (
     <div className={styles.notFoundContainer}>
+      {/* Display the error image */}
+      <img src={errorImage} alt="Error" className={styles.errorImage} />
+
       <div className={styles.notFoundText}>
         <h1 className={styles.notFoundHeading}>Oops! Page not found</h1>
         <p className={styles.notFoundDescription}>
